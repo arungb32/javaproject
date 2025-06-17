46000b225d67e726e467ed1c0be6505f2fb9493e
Arun <Arun@192.168.137.142>
2025-06-17 12:01:57 +0530

    This is a java program

---
+++ Student/src/Student/Student.java
@@ -0,0 +1,12 @@
+package Student;
+
+public class Student {
+
+	public static void main(String[] args) {
+		String Studentname="Arjun";
+		String Studentrollno="101";
+		System.out.println("name="+Studentname);
+		System.out.println("rollno="+Studentrollno);
+	}
+
+}
